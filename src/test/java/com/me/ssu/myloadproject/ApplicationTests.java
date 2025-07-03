9f0bddb7f429f7a39ba15f0b133296e38fef9f8f
김수환 <zzanggoon8@hiconsy.com>
2025-07-03 22:24:43 +0900

    기본 build.gradle 설정

---
+++ src/test/java/com/me/ssu/myloadproject/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.ssu.myloadproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
